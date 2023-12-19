a4def2b5982f7ad60207364f8600a4a6b369bd49
jmarti45 <jmartinez72@hotmail.es>
2023-12-19 14:30:37 +0000

    rggtgt

--- main.tf
+++ main.tf
@@ -51,21 +51,3 @@ resource "azurerm_resource_group" "comun" {
   tags     = var.binding_tags
   provider = azurerm.subscription_2
 }
-resource "azurerm_virtual_network" "vnetcon1" {
-  name                = "vnet-${var.Enterprise1}-${var.Environment}-${var.Location.code}-01"
-  location            = var.Location.name
-  resource_group_name = azurerm_resource_group.comun.name
-  address_space       = [var.Address_space]
-  tags                = var.binding_tags
-  #dns_servers         = ["11.0.0.4", "11.0.0.5"]
-  provider = azurerm.subscription_2
-}
-
-
-
-resource "azurerm_subnet" "vgw" {
-  name                 = "snet-${var.Enterprise1}-${var.Environment}-${var.Location.code}-01"
-  resource_group_name  = azurerm_resource_group.comun.name
-  virtual_network_name = azurerm_virtual_network.vnetcon1.name
-  address_prefixes     = [var.Address_prefixes]
-}
