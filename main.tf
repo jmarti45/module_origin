347c2fd34990acd45782cb7aa1879e019b21190b
jmarti45 <jmartinez72@hotmail.es>
2023-12-20 10:41:56 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,7 +50,6 @@ resource "azurerm_resource_group" "comun" {
   name     = "rg-${var.Enterprise1}-${var.Environment}-${var.Project1}-03"
   location = var.Location.name
   tags     = var.binding_tags
-  provider = azurerm.pre
 }
 
 /*
