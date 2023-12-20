40f841f5f074f7ccce49e92d6dcf3b8baec96f05
jmarti45 <jmartinez72@hotmail.es>
2023-12-20 11:09:17 +0000

    dd32d23d2

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-provider "azurerm" {
-  alias           = "dev"
-  subscription_id = var.dev_sub_id
-  features {}
-}
-
-provider "azurerm" {
-  alias           = "pre"
-  subscription_id = var.pre_sub_id
-  features {}
-}
-
-
 resource "azurerm_resource_group" "comun" {
   name     = "rg-${var.Enterprise1}-${var.Environment}-${var.Project1}-03"
   location = var.Location.name
