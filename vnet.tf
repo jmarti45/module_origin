13f3094f0f0fcbe99f6042afdb40969451f7c890
jmarti45 <jmartinez72@hotmail.es>
2023-12-20 13:59:12 +0000

    ededede

--- vnet.tf
+++ vnet.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "vnetcon1" {
   location            = var.Location.name
   resource_group_name = azurerm_resource_group.comun.name
   address_space       = [var.Address_space]
-  tags                = var.binding_tags
+  #tags                = var.binding_tags
   #dns_servers         = ["11.0.0.4", "11.0.0.5"]
   provider = azurerm.pre
 }
