3b47f6e140b77133ae357e7d91f396a4875ca5b0
jmarti45 <jmartinez72@hotmail.es>
2023-12-20 11:19:06 +0000

    mjmjmjm

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.7.0"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  alias           = "dev"
+  subscription_id = var.dev_sub_id
+  features {}
+}
+
+provider "azurerm" {
+  alias           = "pre"
+  subscription_id = var.pre_sub_id
+  features {}
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+# Location
+variable "dev_sub_id" {
+  type = map(string)
+}
+
+# Location
+variable "pre_sub_id" {
+  type = map(string)
+}
