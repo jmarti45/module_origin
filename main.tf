43b504a1c2907e61b7f95a9b9a89e8bb598a8901
jmarti45 <jmartinez72@hotmail.es>
2023-12-20 11:50:00 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "comun" {
   name     = "rg-${var.Enterprise1}-${var.Environment}-${var.Project1}-03"
   location = var.Location.name
-  tags     = var.binding_tags
+  #tags     = var.binding_tags
 }
